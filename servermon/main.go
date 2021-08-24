fa9797043a281810744156896f20ac537829b050
nigel447 <nigel447@gmail.com>
2021-08-24 11:53:24 +0300

    viper config feature

--- servermon/main.go
+++ servermon/main.go
@@ -10,10 +10,11 @@ import (
 )
 
 func main() {
+	InitConfig()
 	c := make(chan os.Signal)
 	signal.Notify(c,  os.Interrupt, syscall.SIGTERM)
-	srv := server.Server{ Addr: ":2222" }
-	fmt.Println("calling bootstap on", srv.Addr)
+	srv := server.Server{ Addr:config.Address }
+	fmt.Println("calling bootstap on ", srv.Addr)
 	ctx, cancel := context.WithCancel(context.Background())
 
 	go func(){
