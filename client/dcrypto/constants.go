ede67dbdac2631d90a15a2ff003d62d418014ac0
nigel447 <nigel447@gmail.com>
2021-09-14 15:57:33 +0300

    remove redundent key

--- client/dcrypto/constants.go
+++ client/dcrypto/constants.go
@@ -1,11 +1,7 @@
 package dcrypto
 
 const PriKeyPem = `-----BEGIN OPENSSH PRIVATE KEY-----
-b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtz
-c2gtZWQyNTUxOQAAACD59wgphF8jW/K+0j8kPWvDKPWOLoaUPpdeP3k+PmJoOQAA
-AIOaywRCmssEQgAAAAtzc2gtZWQyNTUxOQAAACD59wgphF8jW/K+0j8kPWvDKPWO
-LoaUPpdeP3k+PmJoOQAAAEBpnDxun42XGx95FI4UM1Yd42Rp0BaBLOUTDgy7jTEG
-FPn3CCmEXyNb8r7SPyQ9a8Mo9Y4uhpQ+l14/eT4+Ymg5AAAAAA==
+              put your key here
 -----END OPENSSH PRIVATE KEY-----`
 
-const FINGER_PRINT = `SHA256:+DPedGbb69eU9Fm2ZkODa1UkNlKmqnBLuZvqAqnTo+w`
+const FINGER_PRINT = `put your finger print here`
